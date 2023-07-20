231979fcfb39e168a22a7fd554ad8dbd6095c95b
unlimish <14168376+unlimish@users.noreply.github.com>
2023-07-20 21:10:06 +0900

    Update: scripts

--- Assets/Scripts/SnakeMove.cs
+++ Assets/Scripts/SnakeMove.cs
@@ -1,53 +1,61 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class SnakeMovement : MonoBehaviour
+public class SnakeController : MonoBehaviour
 {
-    public float moveSpeed = 1.0f;
-    public GameObject snakeSegmentPrefab;
+    public GameObject textPrefab;
+    public int maxTexts = 100;
 
-    private List<GameObject> snakeSegments = new List<GameObject>();
-    private string[] unicodeCharacters = new string[0];
+    private string[] texts;
+    private TextMesh[] textMeshes;
+
+    private void Start()
+    {
+        texts = new string[maxTexts];
+        textMeshes = new TextMesh[maxTexts];
+    }
 
     private void Update()
     {
         if (Input.anyKeyDown)
         {
-            AddRandomUnicodeCharacter();
+            AddRandomCharacter();
         }
 
         MoveSnake();
     }
 
-    private void AddRandomUnicodeCharacter()
+    private void AddRandomCharacter()
     {
-        string randomCharacter = char.ConvertFromUtf32(Random.Range(0x21, 0x7F));
-        List<string> tempList = new List<string>(unicodeCharacters);
-        tempList.Add(randomCharacter);
-        unicodeCharacters = tempList.ToArray();
-        CreateSnakeSegment();
+        for (int i = 0; i < maxTexts - 1; i++)
+        {
+            texts[i] = texts[i + 1];
+            textMeshes[i].text = texts[i];
+        }
+
+        char randomChar = (char)Random.Range(32, 127); // Unicode characters from space to DEL
+        texts[maxTexts - 1] = randomChar.ToString();
+        CreateText(maxTexts - 1);
     }
 
     private void MoveSnake()
     {
-        for (int i = snakeSegments.Count - 1; i > 0; i--)
+        for (int i = 0; i < maxTexts; i++)
         {
-            snakeSegments[i].transform.position = snakeSegments[i - 1].transform.position;
-        }
-
-        if (snakeSegments.Count > 0)
-        {
-            snakeSegments[0].transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+            if (textMeshes[i] != null)
+            {
+                textMeshes[i].transform.Translate(Vector3.left * Time.deltaTime);
+            }
         }
     }
 
-    private void CreateSnakeSegment()
+    private void CreateText(int index)
     {
-        GameObject newSegment = Instantiate(snakeSegmentPrefab, Vector3.zero, Quaternion.identity);
-        newSegment.transform.SetParent(transform);
-        newSegment.GetComponentInChildren<TextMesh>().text = unicodeCharacters[unicodeCharacters.Length - 1];
-        snakeSegments.Add(newSegment);
+        GameObject textObject = Instantiate(textPrefab, Vector3.zero, Quaternion.identity);
+        textObject.transform.SetParent(transform);
+        textObject.transform.position = new Vector3(9f, Random.Range(-4.5f, 4.5f), 0f); // Initial position on the right side
+        textObject.GetComponent<TextMesh>().text = texts[index];
+        textMeshes[index] = textObject.GetComponent<TextMesh>();
     }
 }
 
